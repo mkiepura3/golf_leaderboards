405950f53c2e73e939eeae92062e4db722f731ae
mkiepura3 <mkiepura2017@kellogg.northwestern.edu>
2016-06-10 23:50:03 -0500

    Rev 4.0 - matt-kiepura-golf-leaderboards
    Final save with app working - for Final Project submission grading.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,24 +1,24 @@
 class Course < ActiveRecord::Base
-  validates :eventname, :presence => true, :uniqueness => true
+  validates :coursename, :presence => true, :uniqueness => true
   validates :yardage, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1000, :less_than_or_equal_to => 8000 }
-  validates :parhole1, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole2, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole3, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole4, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole5, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole6, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole7, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole8, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole9, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole10, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole11, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole12, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole13, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole14, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole15, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole16, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole17, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :parhole18, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
+  validates :parhole1, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole2, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole3, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole4, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole5, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole6, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole7, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole8, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole9, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole10, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole11, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole12, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole13, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole14, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole15, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole16, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole17, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :parhole18, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
 
   has_many :scores
   has_many :groupevents
--- app/models/score.rb
+++ app/models/score.rb
@@ -2,24 +2,24 @@ class Score < ActiveRecord::Base
   validates :golfer_id, :presence => true
   validates :course_id, :presence => true
   validates :date, :presence => true
-  validates :hole1, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole2, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole3, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole4, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole5, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole6, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole7, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole8, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole9, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole10, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole11, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole12, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole13, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole14, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole15, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole16, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole17, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
-  validates :hole18, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15 }
+  validates :hole1, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole2, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole3, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole4, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole5, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole6, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole7, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole8, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole9, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole10, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole11, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole12, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole13, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole14, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole15, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole16, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole17, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
+  validates :hole18, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 15, :allow_blank => true }
 
   belongs_to :golfer
   belongs_to :course
